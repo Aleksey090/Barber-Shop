35fd9a804f6aec57b54010759be402503b9acd09
Aleksey090 <yurevi44@gmail.com>
2024-06-07 23:23:31 +0300

    Additions from lesson 23

--- app.rb
+++ app.rb
@@ -6,9 +6,60 @@ get '/' do
 end
 
 get '/about' do
+	# @error = "Error"
 	erb :about
 end
 
 get '/visit' do
 	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@color = params[:color]
+	@barber = params[:barber]
+
+				# -----REFACTORING 01-------
+		# if @username == ''
+		# 	@error = 'Enter name'
+		# end
+
+		# if @phone == ''
+		# 	@error = 'Enter phone'
+		# end
+
+		# if @datetime == ''
+		# 	@error = 'Enter time'
+		# end
+
+		# if @error != ''
+		# 	return erb :visit
+		# end
+
+				# -----REFACTORING 02-------
+		hh = { :username => 'Enter name',
+				:phone => 'Enter phone',
+				:datetime => 'Enter time' }
+		
+			@error = hh.select {|key,_| params[key] == ''}.values.join(' & ')
+
+				if @error != ''
+					return erb :visit
+				end
+
+				# -----REFACTORING 03-------
+		# hh.each do |key, value|
+		# 	if params[key] == ''
+		# 		@error = [value]
+		# 		return erb :visit
+		# 	end
+		# end
+
+
+
+	erb "OK, your name: #{@username}, your phone: #{@phone}, date: #{@datetime} and your color: #{@color}"
+
 end
